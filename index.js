ca8ace83b75fc322a82b1efc18e74a09e6a2d978
shagors <shajjadshagor@gmail.com>
2022-05-08 01:36:49 +0600

    delete api add

--- index.js
+++ index.js
@@ -35,6 +35,21 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
         });
+
+        app.get('/products/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally{
 
