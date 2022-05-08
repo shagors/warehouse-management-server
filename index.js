6f6a157ef141d0d9a39a0de8550d1a44c92dc2f4
shagors <shajjadshagor@gmail.com>
2022-05-08 18:58:55 +0600

    env file add and checked and reme.md file add

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ function verifyJWT(req, res, next){
     next();
 }
 
-const uri = "mongodb+srv://warehouseDB:QpIwV5rIfsGbeg7p@cluster0.fooxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fooxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
