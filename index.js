48864daafaf9ce43bec6b4d4af6e49cdf9d1a504
shagors <shajjadshagor@gmail.com>
2022-05-08 18:46:34 +0600

    all files upload

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const {
     MongoClient,
     ServerApiVersion,
     ObjectId
 } = require('mongodb');
-require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -16,6 +16,21 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'})
+        }
+        req.decoded = decoded;
+    })
+    next();
+}
 
 const uri = "mongodb+srv://warehouseDB:QpIwV5rIfsGbeg7p@cluster0.fooxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
@@ -28,6 +43,15 @@ async function run() {
         try {
             await client.connect();
             const productCollection = client.db('warehouse').collection('products');
+
+            // JWT Auth
+            app.post('/login', async(req,res) => {
+                const user = req.body;
+                const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                    expiresIn: '2d'
+                });
+                res.send({accessToken});
+            })
 
         app.get('/products', async(req, res) => {
             const query = {};
@@ -50,6 +74,12 @@ async function run() {
             res.send(result);
         });
 
+        // POST
+        app.post('/products', async(req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        });
     }
     finally{
 
